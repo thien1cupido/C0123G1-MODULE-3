17a09a27359239934e673d2f3682647a5a105a84
thien1cupido <thien7028@gmail.com>
2023-04-18 00:04:44 +0700

    sửa case study

--- case_study_moddule_3_lan_1/sql_cau_16_20_case_study.sql
+++ case_study_moddule_3_lan_1/sql_cau_16_20_case_study.sql
@@ -1,8 +1,12 @@
 USE furama;
 
 -- 16.Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
-SELECT *FROM hop_dong
-J
-   WHERE hop_dong.ma_nhan_vien = nhan_vien.
-   
-    AND hop_dong.ngay_ky BETWEEN '2019-01-01' AND '2021-12-31'
+DELETE FROM nhan_vien nv
+WHERE nv.ma_nhan_vien NOT IN (SELECT nv.ma_nhan_vien FROM nhan_vien nv
+JOIN hop_dong hd ON hd.ma_nhan_vien=nv.ma_nhan_vien
+WHERE year(hd.ngay_lam_hop_dong) BETWEEN 2019 AND 2021);
+
+-- 17.Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond, 
+-- chỉ cập nhật những khách hàng đã từng đặt phòng với Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
+
+SELECT *FROM loai_khach
